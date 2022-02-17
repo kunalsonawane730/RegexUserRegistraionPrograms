bfffb6e4be3b1940ff35ef1c7cc99f180c5426bb
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-17 22:56:07 +0530

    [kunal]commiting changes for enter user emailId program

--- RegexUserRegistrationProgram/RegexUserRegistrationProgram/RegistrationOfUser.cs
+++ RegexUserRegistrationProgram/RegexUserRegistrationProgram/RegistrationOfUser.cs
@@ -22,18 +22,31 @@ namespace RegexUserRegistrationProgram
             {
                 Console.WriteLine("Your entered name is in invalid pattern");
             }
-            Regex LastName = new Regex("^[A-Z]{1}[a-z]{2,}$");
 
-            Console.WriteLine("Enter Last Name :");
+            Regex LastName = new Regex("^[A-Z]{1}[a-z]{2,}$");
+            Console.WriteLine("Enter your Last Name:");
             string lastname = Console.ReadLine();
             if (LastName.IsMatch(lastname))
             {
-                Console.WriteLine("Your last Name is :" + lastname);
+                Console.WriteLine("Your last Name is:" + lastname);
             }
             else
             {
                 Console.WriteLine("Your entered last name is in invalid pattern");
             }
+            Regex EmailId = new Regex("^[a-z]{1,}[.][a-z]{1,}[0-9]{0,}[@][a-z]{1,}[.][a-z]{1,3}[.]{0,}[a-z]{0,2}$");
+
+            Console.WriteLine("Enter your EmailId: ");
+            string emailid = Console.ReadLine();
+            if (EmailId.IsMatch(emailid))
+            {
+                Console.WriteLine("Your emailId is:" +emailid);
+            }
+            else
+            {
+                Console.WriteLine("Your entered emailId is in invalid pattern");
+            }
+
         }
     }
 }
