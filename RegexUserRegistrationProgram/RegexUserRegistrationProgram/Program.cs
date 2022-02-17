5d6b0f1e57f69e7ee0817c9921b390c16fc4d7f0
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-17 22:05:57 +0530

    [kunal]commiting changes for user first name validation

---
+++ RegexUserRegistrationProgram/RegexUserRegistrationProgram/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RegexUserRegistrationProgram;
+//Console.WriteLine("Hello, World!");
+RegistrationOfUser user = new RegistrationOfUser();
+user.UserRegistration();
---
+++ RegexUserRegistrationProgram/RegexUserRegistrationProgram/RegistrationOfUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexUserRegistrationProgram
+{
+    public class RegistrationOfUser
+    {
+        public void UserRegistration()
+        {
+            Regex firstName = new Regex("^[A-Z]{1}[a-z]{2,}$");
+            Console.WriteLine("Enter your first Name :");
+            string firstname = Console.ReadLine();
+            if (firstName.IsMatch(firstname))
+            {
+                Console.WriteLine("Your Name is :" + firstname);
+            }
+            else
+            {
+                Console.WriteLine("Your entered name is in invalid pattern");
+            }
+        }
+    }
+}
